0569acd398342b1b14aa7049ecbb0eb80d8f905a
wallace <sansanoskiww@gmail.com>
2024-03-07 11:40:25 -0300

    download

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -7,7 +7,7 @@ function About  () {
             <div className={style.containerDescription}>
                 <p className={style.introduction}>developer front-end junior with experience using javascript, react, redux, node js, html, css, jest and firebase. Passionate about technology and share new ideas to improve web development.</p>
             </div>
-            <a href='#' download >baixar  cs</a>
+            <a href='./file/cv.pdf' download >baixar  cs</a>
         </div>
     )
 }
