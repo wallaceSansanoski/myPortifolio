6525da2d57796e5cd6ffed25d871167be3cd40f9
wallace <sansanoskiww@gmail.com>
2024-03-07 17:59:35 -0300

    adding effectbreaking

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,13 +1,12 @@
 import style from './about.module.css'
 
-function About  () {
+function About () {
     return (
         <div className={style.aboutContainer}>
             <h1 className={style.title}>about</h1>
             <div className={style.containerDescription}>
-                <p className={style.introduction}>developer front-end junior with experience using javascript, react, redux, node js, html, css, jest and firebase. Passionate about technology and share new ideas to improve web development.</p>
-            </div>
-            <a href='./src/pages/about/cv.pdf' download >baixar  cs</a>
+               <p className={style.introduction}>developer front-end junior with experience using javascript, react, redux, node js, html, css, jest and firebase. Passionate about technology and share new ideas to improve web development.</p>
+           </div>
         </div>
     )
 }
